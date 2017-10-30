ba02dcc28268ca1090e34dfe51d595e57f2889eb
wkurmann <willy.kurmann@gmail.com>
2017-10-30 21:40:30 +0100

    Added variables.py

---
+++ PythonAutomation/variables.py
@@ -0,0 +1,13 @@
+
+i = 10 # int
+j = 20.20 # float
+k = "testing world" #string
+
+print(i)
+print(j)
+print(k)
+
+del i # delete a variable
+
+i = int("10") # type casting
+print("casting int: '10'={}".format(i))
