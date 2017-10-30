7b004690ea65ddc3a944d3321b4d0db781342734
wkurmann <willy.kurmann@gmail.com>
2017-10-30 21:30:45 +0100

    Updated start.py

--- PythonAutomation/start.py
+++ PythonAutomation/start.py
@@ -4,7 +4,7 @@ print("Welcome to Python programming\n")
 
 name = input("please enter your name:")
 
-# \ is a continuous symbol used to write statement on multiple lines
+# \ (Continuous symbol) to write statement on multiple lines
 # ; for multiple statements in single line
 greetings = \
     "Hello, {}!".format(name); print("\n" + greetings)
