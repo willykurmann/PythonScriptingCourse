3458e39f16a97908685579f2b5e133499c289920
wkurmann <willy.kurmann@gmail.com>
2017-10-30 21:20:45 +0100

    Kickoff

---
+++ PythonAutomation/start.py
@@ -0,0 +1,7 @@
+# this is python comment
+
+print("Welcome to Python programming\n")
+
+
+name = input("please enter your name:")
+print("your name is {}".format(name)) # Python3 string formatting (better than +)
