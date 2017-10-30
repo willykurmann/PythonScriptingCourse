913c217311bd2cf86559f4614f3858f00a1f078a
wkurmann <willy.kurmann@gmail.com>
2017-10-30 22:10:48 +0100

    Added lists.py

---
+++ PythonAutomation/lists.py
@@ -0,0 +1,12 @@
+li = [23,34.2, "string value"] # list can contain items of multiple types
+print(li)
+print(len(li))
+print(li[0])
+print(li[0:2])
+
+li[0] = 666 # Change value in list
+
+# Concat list
+li1 = [23,54.2, "string value2"]
+li2 = li + li1
+print(li2)
