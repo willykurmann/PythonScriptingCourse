b2959ed15751914e5b69d86fed97e2dee5b90caa
wkurmann <willy.kurmann@gmail.com>
2017-10-31 22:27:23 +0100

    Added modules

---
+++ PythonAutomation/module1.py
@@ -0,0 +1,11 @@
+print("This is module direct execution line")
+
+def module_function():
+    print("This is module function")
+
+class ModuleClass:
+    def __init__(self):
+        print("This is class constructor")
+    
+    def class_method(self):
+        print("This is class method")
---
+++ PythonAutomation/modules.py
@@ -0,0 +1,5 @@
+import module1
+module1.module_function()
+
+ob = module1.ModuleClass()
+ob.class_method()
